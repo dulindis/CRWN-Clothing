2ad856f27922545ffa4f41d53d89c7ec602a9bff
POkulska <paulina.okulska@gmail.com>
2021-05-10 14:01:27 +0200

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 import {auth} from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser,hidden}) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo'/>
@@ -17,10 +20,20 @@ const Header = ({currentUser}) => (
             <div className="option" onClick={()=>auth.signOut()}>SIGN OUT</div>
             :
             <Link className="option" to='/signIn'>SIGN IN</Link>
-        }
+            }
+        <CartIcon/>
         </div>
+       {
+           hidden ? null : 
+           <CartDropdown/>
+       }
 
     </div>
-)
+);
 
-export default Header
+const mapStateToProps = ({user:{currentUser},cart:{hidden}}) => ({
+    currentUser,
+    hidden,    
+})
+
+export default connect(mapStateToProps)(Header)
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,15 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+  if (existingCartItem) {
+    //we use map because we need to reutnr a new array to rerender
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
