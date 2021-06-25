3d334270b08beb5d5520af2ba5df5ac581ba3ecd
POkulska <paulina.okulska@gmail.com>
2021-06-25 11:46:11 +0200

    added container HOC for CartDropdown

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
-import {withRouter} from 'react-router-dom';
-
-// import CustomButton from '../custom-button/custom-button.component';
-// import './cart-dropdown.styles.scss';
 import CartItem from '../cart-item/cart-item.component';
 
 import { toggleCartHidden } from "../../redux/cart/cart-actions";
-import {selectCartItems}  from '../../redux/cart/cart.selectors';
 
 import {
     CartDropdownContainer,
@@ -35,11 +28,4 @@ const CartDropdown = ({cartItems,history,dispatch}) => (
     </CartDropdownContainer>
 )
 
-
-const mapStateToProps = createStructuredSelector({
-    cartItems:selectCartItems
-});
-
-
-  
-export default withRouter(connect(mapStateToProps)(CartDropdown));
+export default CartDropdown
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,7 +5,8 @@ import { createStructuredSelector } from "reselect";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../cart-icon/cart-icon.component";
-import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import CartDropdownContainer from '../cart-dropdown/cart-dropdown.container';
+// import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { selectCurrentUser } from "../../redux/user/user.selectors";
@@ -40,7 +41,7 @@ const Header = ({ currentUser, hidden }) => (
       )}
       <CartIcon />
     </OptionsContainer>
-    {hidden ? null : <CartDropdown />}
+    {hidden ? null : <CartDropdownContainer />}
   </HeaderContainer>
 );
 
